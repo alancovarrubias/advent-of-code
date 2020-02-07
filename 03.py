4b31e21e499d9de002f1fdb4b2069dd461adc8af
Alan Covarrubias <aecovarru@gmail.com>
2020-02-07 03:44:35 -0800

    add distance traveled

--- 03.py
+++ 03.py
@@ -1,16 +1,20 @@
 SEPARATOR = ","
 FILENAME = "files/03.txt"
 
-def calculate_distance(intersection):
+def distance_traveled(intersection):
+    return intersection[2] + intersection[3]
+
+def manhattan_distance(intersection):
     return abs(intersection[0]) + abs(intersection[1])
 
 def find_intersections(wire1, wire2):
     intersections = []
-    p2 = [0, 0]
+    p2 = [0, 0, 0]
     for dir1 in wire1:
         p1 = p2.copy()
         p2[0] += dir1[0]
         p2[1] += dir1[1]
+        p2[2] += max(abs(dir1[0]), abs(dir1[1]))
 
         if p1[0] == p2[0]:
             static_index = 0
@@ -18,19 +22,24 @@ def find_intersections(wire1, wire2):
         elif p1[1] == p2[1]:
             static_index = 1
             dynamic_index = 0
-        q2 = [0, 0]
+
+        q2 = [0, 0, 0]
         for dir2 in wire2:
             q1 = q2.copy()
             q2[0] += dir2[0]
             q2[1] += dir2[1]
+            q2[2] += max(abs(dir2[0]), abs(dir2[1]))
 
-            intersection = [0, 0]
+            # [x, y, d1, d2]
+            intersection = [0, 0, 0, 0]
             intersection[static_index] = p1[static_index]
             if ((q1[static_index] < p1[static_index] and q2[static_index] > p1[static_index]) or
                 (q1[static_index] > p1[static_index] and q2[static_index] < p1[static_index])):
                 intersection[dynamic_index] = q2[dynamic_index]
                 if ((p1[dynamic_index] < q2[dynamic_index] and p2[dynamic_index] > q2[dynamic_index]) or
                     (p1[dynamic_index] > q2[dynamic_index] and p2[dynamic_index] < q2[dynamic_index])):
+                    intersection[2] = p1[2] + abs(p2[0] - p1[0]) + abs(p2[1] - p1[1])  
+                    intersection[3] = q1[2] + abs(q2[0] - q1[0]) + abs(q2[1] - q1[1]) 
                     intersections.append(intersection)
         return intersections
 
@@ -61,5 +70,7 @@ def build_wires(filename):
 
 wire1, wire2 = build_wires(FILENAME)
 intersections = find_intersections(wire1, wire2)
-shortest_distance = min(list(map(calculate_distance, intersections)))
-print(shortest_distance)
+manhattan_distances = list(map(manhattan_distance, intersections))
+distances_traveled = list(map(distance_traveled, intersections))
+print(manhattan_distances)
+print(distances_traveled)
